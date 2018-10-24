a0c0600d7cbc4b0938ff8e24c13c4b844b49ba51
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-10-24 11:19:54 +0300

    App component added

--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -3,6 +3,13 @@
 import React from 'react';
 
 export default class App extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false
+        };
+    }
+
     render() {
         return (
             <div></div>
